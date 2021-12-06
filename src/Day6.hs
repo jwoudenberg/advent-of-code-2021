669eae33d79daeb33a95507c7438084fa1ee10b2
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-12-06 08:27:52 +0100

    Day 6, Part 2

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,26 +1,41 @@
 module Day6 (main) where
 
 import Data.Function ((&))
+import Data.List (group, sort)
+import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
 import qualified Data.Text.IO
 
 main :: IO ()
 main = do
   input <- parse
-  population 80 input
-    & length
+  groupByAge input
+    & population 256
+    & sum
     & print
+
+groupByAge :: [Int] -> [Int]
+groupByAge fishes =
+  let ageAndCount =
+        fishes
+          & sort
+          & group
+          & map (\ageCohort -> (head ageCohort, length ageCohort))
+   in fmap
+        (\age -> fromMaybe 0 (lookup age ageAndCount))
+        [0 .. (maximum (fmap fst ageAndCount))]
 
 population :: Int -> [Int] -> [Int]
 population 0 prev = prev
 population day prev = population (day - 1) (oneDayLater prev)
 
 oneDayLater :: [Int] -> [Int]
-oneDayLater fishes =
-  fishes & concatMap ageFish
-  where
-    ageFish 0 = [6, 8]
-    ageFish n = [n - 1]
+oneDayLater [] = []
+oneDayLater (age0 : otherAges) =
+  zipWith
+    (+)
+    (otherAges ++ repeat 0)
+    [0, 0, 0, 0, 0, 0, age0, 0, age0]
 
 parse :: IO [Int]
 parse = do
