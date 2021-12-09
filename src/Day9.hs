db9e02151e92c6afdbbf2ff98a2ec17534dc90ca
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-12-09 09:51:29 +0100

    Day 9, Part 2

--- src/Day9.hs
+++ src/Day9.hs
@@ -2,30 +2,59 @@ module Day9 (main) where
 
 import Data.Char (digitToInt)
 import Data.Function ((&))
-import Data.List (zipWith5)
-import Data.Maybe (catMaybes)
+import Data.List (sort)
+import qualified Data.Map.Strict as Map
+import Data.Maybe (catMaybes, fromMaybe)
+import qualified Data.Set as Set
 import qualified System.IO
 
 main :: IO ()
 main = do
   input <- parse
-  let width = length (head input)
-  let datapoints = concat input
-  let borderVal = 100
-  let leftNeighbours = drop 1 datapoints ++ repeat borderVal
-  let rightNeighbours = borderVal : datapoints
-  let topNeighbours = drop width datapoints ++ repeat borderVal
-  let bottomNeighbours = replicate width borderVal ++ datapoints
-  zipWith5 dangerVal datapoints leftNeighbours topNeighbours rightNeighbours bottomNeighbours
-    & catMaybes
-    & sum
+  let terrain =
+        zip [0 :: Int ..] input
+          & concatMap (\(i, row) -> zipWith (\j depth -> ((i, j), depth)) [0 :: Int ..] row)
+          & Map.fromList
+  minima terrain
+    & fmap (Set.size . basinAround terrain . fst)
+    & sort
+    & reverse
+    & take 3
+    & product
     & print
 
-dangerVal :: Int -> Int -> Int -> Int -> Int -> Maybe Int
-dangerVal point left top right bottom =
-  if point < left && point < top && point < right && point < bottom
-    then Just (point + 1)
-    else Nothing
+type Coord = (Int, Int)
+
+minima :: Map.Map Coord Int -> [(Coord, Int)]
+minima terrain =
+  Map.toList terrain
+    & fmap
+      ( \(coord, val) ->
+          if all ((> val) . heightAt terrain) (neighbours coord)
+            then Just (coord, val)
+            else Nothing
+      )
+    & catMaybes
+
+heightAt :: Map.Map Coord Int -> Coord -> Int
+heightAt terrain coord = Map.lookup coord terrain & fromMaybe 9
+
+neighbours :: Coord -> [Coord]
+neighbours (x, y) = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+
+basinAround :: Map.Map Coord Int -> Coord -> Set.Set Coord
+basinAround terrain coord =
+  expand (Set.singleton coord)
+  where
+    expand :: Set.Set Coord -> Set.Set Coord
+    expand current =
+      let next =
+            Set.toList current
+              & concatMap neighbours
+              & Set.fromList
+              & Set.union current
+              & Set.filter ((< 9) . heightAt terrain)
+       in if next == current then current else expand next
 
 parse :: IO [[Int]]
 parse = do
