4ca19108fc14e642bff42c9ba4abaa3b33459a95
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-12-10 08:39:42 +0100

    Day 10, Part 2

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,34 +1,51 @@
 module Day10 (main) where
 
 import Data.Function ((&))
+import Data.List (sort)
 import qualified System.IO
 
 main :: IO ()
 main = do
   input <- parse
   input
-    & fmap failingScore
-    & sum
+    & fmap completeScore
+    & filter (> 0)
+    & mean
     & print
 
-failingScore :: String -> Int
-failingScore line = go line ""
+completeScore :: String -> Int
+completeScore line = go line ""
   where
     go :: String -> String -> Int
     -- End of input
-    go [] _ = 0
+    go [] opened = closeAll opened
     -- Legal closing of a chunk
     go (')' : rest) ('(' : opened) = go rest opened
     go (']' : rest) ('[' : opened) = go rest opened
     go ('}' : rest) ('{' : opened) = go rest opened
     go ('>' : rest) ('<' : opened) = go rest opened
     -- Ilegal closing of a chunk
-    go (')' : _) _ = 3
-    go (']' : _) _ = 57
-    go ('}' : _) _ = 1197
-    go ('>' : _) _ = 25137
+    go (')' : _) _ = 0
+    go (']' : _) _ = 0
+    go ('}' : _) _ = 0
+    go ('>' : _) _ = 0
     -- Opening a chunk
     go (opener : rest) opened = go rest (opener : opened)
+
+closeAll :: String -> Int
+closeAll = go 0
+  where
+    go :: Int -> String -> Int
+    go score [] = score
+    go score ('(' : rest) = go (1 + 5 * score) rest
+    go score ('[' : rest) = go (2 + 5 * score) rest
+    go score ('{' : rest) = go (3 + 5 * score) rest
+    go score ('<' : rest) = go (4 + 5 * score) rest
+    go _ _ = undefined
+
+mean :: Ord a => [a] -> a
+mean xs =
+  sort xs !! (length xs `div` 2)
 
 parse :: IO [String]
 parse = do
