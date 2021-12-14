a9a4382b9b0bbb3b481645cdf9cd65a8259f0819
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-12-14 08:57:24 +0100

    Day 14, Part 2

--- src/Day14.hs
+++ src/Day14.hs
@@ -3,6 +3,7 @@ module Day14 (main) where
 import Data.Function ((&))
 import Data.List (group, sort)
 import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
 import qualified Data.Text.IO
 
@@ -10,29 +11,43 @@ main :: IO ()
 main = do
   (template, rules_) <- parse
   let rules = Map.fromList (rules_)
-  steps 10 rules template
+  let pairs =
+        zip template (tail template)
+          & sort
+          & group
+          & fmap (\g -> (head g, length g))
+          & Map.fromList
+  steps 40 rules pairs
+    & Map.toList
+    & concatMap (\((a, b), count) -> [Map.singleton a count, Map.singleton b count])
+    -- All elements are in two pairs, except the first and the last.
+    & (Map.singleton (head template) 1 :)
+    & (Map.singleton (last template) 1 :)
+    & foldr (Map.unionWith (+)) mempty
+    & fmap (\n -> n `div` 2)
+    & Map.elems
     & sort
-    & group
-    & fmap length
-    & sort
-    & (\res -> last res - head res)
+    & (\res -> (last res - head res))
     & print
 
 type Rules = Map.Map (Char, Char) Char
 
-steps :: Int -> Rules -> String -> String
+type Pairs = Map.Map (Char, Char) Int
+
+steps :: Int -> Rules -> Pairs -> Pairs
 steps 0 _ input = input
 steps n rules input =
-  zip input (tail input)
-    & foldr insert ""
-    & (:) (head input)
+  foldr insert mempty (Map.toList input)
     & steps (n - 1) rules
   where
-    insert :: (Char, Char) -> [Char] -> [Char]
-    insert pair@(_, after) acc =
+    insert :: ((Char, Char), Int) -> Pairs -> Pairs
+    insert (pair@(before, after), count) pairs =
       case Map.lookup pair rules of
-        Nothing -> after : acc
-        Just new -> new : after : acc
+        Nothing -> pairs
+        Just new ->
+          pairs
+            & Map.alter (Just . (+ count) . fromMaybe 0) (before, new)
+            & Map.alter (Just . (+ count) . fromMaybe 0) (new, after)
 
 parse :: IO (String, [((Char, Char), Char)])
 parse = do
